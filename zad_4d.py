461a6dd09b91bbf218ad6777faffead334e40e2e
Elfi2006 <wiktorjaskolka@interia.pl>
2025-11-16 22:41:45 +0100

    feat: Finalne scalenie zadan 1-4 na python_basic i korekta struktury plikow

---
+++ zad_4d.py
@@ -0,0 +1,18 @@
+# zad_2d.py
+
+def co_drugi_element(moja_lista):
+    print("Co drugi element z listy:")
+
+    # Lepiej zrobic to tak, zeby bylo widac, ze sie nie uzywa slicing
+    index = 0
+    while index < len(moja_lista):
+        print(moja_lista[index])
+        index = index + 2  # Licznik co 2
+
+
+# Testowanie
+lista_wejsciowa = []
+for k in range(10):
+    lista_wejsciowa.append(k)
+
+co_drugi_element(lista_wejsciowa)

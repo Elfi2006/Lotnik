8945898670c415b86df459e4214f8c0832f4b7d7
Elfi2006 <wiktorjaskolka@interia.pl>
2025-12-25 14:09:06 +0100

    Update zad_4d.py

--- zad_4d.py
+++ zad_4d.py
@@ -4,14 +4,14 @@
 def co_drugi_element(moja_lista):
     print("Co drugi element z listy:")
 
-    # Lepiej zrobic to tak, zeby bylo widac, ze sie nie uzywa slicing
+    
     index = 0
     while index < len(moja_lista):
         print(moja_lista[index])
         index = index + 2  # Licznik co 2
 
 
-# Testowanie
+
 lista_wejsciowa = []
 for k in range(10):
     lista_wejsciowa.append(k)
